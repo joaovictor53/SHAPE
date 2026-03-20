1bb3ab41a45800ca0731275902c0dcd91d0a7d79
joao53 <victors.rpe@gmail.com>
2026-03-20 11:57:33 -0400

    feat: add dotenv and fastify config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World !!");
+import "dotenv/config";
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT ?? 8080) });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
